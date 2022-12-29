0bd1eb4749da840aba16a97b16f7786a2bfd0622
JoshuaLudig <joshludig45@yahoo.com>
2022-12-28 20:52:46 -0800

    Just putting classes in different folders to clean up a bit

--- Application/src/main/java/com/kenzie/appserver/controller/SongDownloadController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/SongDownloadController.java
@@ -1,19 +1,14 @@
-package com.kenzie.appserver.repositories.model;
+package com.kenzie.appserver.controller;
 
-import com.kenzie.appserver.controller.model.ExampleCreateRequest;
-import com.kenzie.appserver.controller.model.ExampleResponse;
-import com.kenzie.appserver.service.ExampleService;
+import com.kenzie.appserver.controller.model.GetSongDownloadResponse;
+import com.kenzie.appserver.controller.model.SongDownloadResponse;
 
 import com.kenzie.appserver.service.NewMusicFeaturesService;
-import com.kenzie.appserver.service.model.Example;
 import com.kenzie.appserver.service.model.SongInfo;
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-
-import static java.util.UUID.randomUUID;
 
 @RestController
 @RequestMapping("song")
