7233ca042722e5d8ed875a41ce91651dbc8c4e7c
JoshuaLudig <joshludig45@yahoo.com>
2023-01-07 01:41:59 -0800

    Refixed the overrides in the NewMusicFeaturesRecord class

--- Application/src/main/java/com/kenzie/appserver/repositories/model/NewMusicFeaturesRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/NewMusicFeaturesRecord.java
@@ -1,5 +1,8 @@
 package com.kenzie.appserver.repositories.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+
 public class NewMusicFeaturesRecord {
 private String featureId;
 private String featureName;
@@ -23,56 +26,57 @@ private String songId;
 
    public void setSongId(String songId) {
        this.songId = songId;
-//    }
-//    @DynamoDBAttribute(attributeName = "featureDescription")
-//    public String getFeatureName() {
-//        return featureName;
-//    }
-//
-//    public void setFeatureName(String featureName) {
-//        this.featureName = featureName;
-//    }
-//    @DynamoDBAttribute(attributeName = "featureDescription")
-//    public String getFeatureDescription() {
-//        return featureDescription;
-//    }
-//
-//    public void setFeatureDescription(String featureDescription) {
-//        this.featureDescription = featureDescription;
-//    }
-//    @DynamoDBAttribute(attributeName = "featureImage")
-//    public String getFeatureImage() {
-//        return featureImage;
-//    }
-//
-//    public void setFeatureImage(String featureImage) {
-//        this.featureImage = featureImage;
-//    }
-//    @DynamoDBAttribute(attributeName = "featureLink")
-//    public String getFeatureLink() {
-//        return featureLink;
-//    }
-//
-//    public void setFeatureLink(String featureLink) {
-//        this.featureLink = featureLink;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!( o instanceof NewMusicFeaturesRecord )) return false;
-//        NewMusicFeaturesRecord that = (NewMusicFeaturesRecord) o;
-//        return Objects.equals(getFeatureId(),
-//                that.getFeatureId()) && Objects.equals(getFeatureName(),
-//                that.getFeatureName()) && Objects.equals(getFeatureDescription(),
-//                that.getFeatureDescription()) && Objects.equals(getFeatureImage(),
-//                that.getFeatureImage()) && Objects.equals(getFeatureLink(),
-//                that.getFeatureLink());
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(getFeatureId(), getFeatureName(), getFeatureDescription(), getFeatureImage(), getFeatureLink());
-//    }
-//}
-//
+    }
+    @DynamoDBAttribute(attributeName = "featureDescription")
+    public String getFeatureName() {
+        return featureName;
+    }
+
+    public void setFeatureName(String featureName) {
+        this.featureName = featureName;
+    }
+    @DynamoDBAttribute(attributeName = "featureDescription")
+    public String getFeatureDescription() {
+        return featureDescription;
+    }
+
+    public void setFeatureDescription(String featureDescription) {
+        this.featureDescription = featureDescription;
+    }
+    @DynamoDBAttribute(attributeName = "featureImage")
+    public String getFeatureImage() {
+        return featureImage;
+    }
+
+    public void setFeatureImage(String featureImage) {
+        this.featureImage = featureImage;
+    }
+    @DynamoDBAttribute(attributeName = "featureLink")
+    public String getFeatureLink() {
+        return featureLink;
+    }
+
+    public void setFeatureLink(String featureLink) {
+        this.featureLink = featureLink;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        NewMusicFeaturesRecord rhs = (NewMusicFeaturesRecord) obj;
+        return songId.equals(rhs.songId);
+    }
+
+    @Override
+    public int hashCode() {return songId.hashCode();
+    }
+}
+
