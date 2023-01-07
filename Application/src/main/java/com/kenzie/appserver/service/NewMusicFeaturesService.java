031fa80a3f74e7aaf876c8f3c2f1c56fc4d4c4a9
JoshuaLudig <joshludig45@yahoo.com>
2023-01-07 01:24:23 -0800

    Fixed NewMusicFeaturesRecord and started NewMusicFeaturesService

--- Application/src/main/java/com/kenzie/appserver/service/NewMusicFeaturesService.java
+++ Application/src/main/java/com/kenzie/appserver/service/NewMusicFeaturesService.java
@@ -1,66 +1,34 @@
-package com.kenzie.appserver.service;
-
-
-import com.kenzie.appserver.repositories.model.NewMusicFeaturesRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class NewMusicFeaturesService {
-
-    private NewMusicFeaturesRepository newMusicFeaturesRepository;
-    private NewMusicFeatures songId;
-    private NewMusicFeatures NewMusicFeatures;
-
-    /**
-     * @param newMusicFeaturesRepository the repository to use
-     * @param newMusicFeatures
-     */
-    public NewMusicFeaturesService(NewMusicFeaturesRepository newMusicFeaturesRepository,
-                                   NewMusicFeaturesService.NewMusicFeatures newMusicFeatures) {
-        this.newMusicFeaturesRepository = newMusicFeaturesRepository;
-        NewMusicFeatures = newMusicFeatures;
-    }
-
-    /**
-     * @param songId the songId to use
-     * @return the song info
-     */
-
-    private NewMusicFeaturesService(NewMusicFeaturesRepository newMusicFeaturesRepository,
-                                    NewMusicFeatures songId, NewMusicFeaturesService.NewMusicFeatures newMusicFeatures) {
-
-        this.songId = songId;
-        NewMusicFeatures = newMusicFeatures;
-    }
-
-    /**
-     * @return the song info
-     */
-    public static NewMusicFeaturesService createNewMusicFeaturesService() {
-        return createNewMusicFeaturesService(null, null, null);
-    }
-
-    public static NewMusicFeaturesService createNewMusicFeaturesService
-            (NewMusicFeaturesRepository newMusicFeaturesRepository, NewMusicFeaturesService.NewMusicFeatures songId,
-             NewMusicFeaturesService.NewMusicFeatures newMusicFeatures) {
-        return new NewMusicFeaturesService(newMusicFeaturesRepository, songId, newMusicFeatures);
-    }
-
-    public NewMusicFeatures findByFeatureId(String id) {
-        findByFeatureId(id);
-        return findByFeatureId(id);
-
-    }
-
-    public String findBySongId(String id) {
-        return id;
-    }
-
-    public <NewMusicFeatures> NewMusicFeatures getNewMusicFeaturesRecord() {
-        return (NewMusicFeatures) NewMusicFeatures;
-    }
-
-    public class NewMusicFeatures {
-    }
-}
-
+//package com.kenzie.appserver.service;
+//
+//
+//import com.kenzie.appserver.repositories.model.NewMusicFeaturesRepository;
+//import com.kenzie.appserver.service.model.NewMusicFeatures;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class NewMusicFeaturesService {
+//
+//    private NewMusicFeaturesRepository newMusicFeaturesRepository;
+//
+//    /**
+//     * @param newMusicFeaturesRepository the repository to use
+//     */
+//    public NewMusicFeaturesService(NewMusicFeaturesRepository newMusicFeaturesRepository) {
+//        this.newMusicFeaturesRepository = newMusicFeaturesRepository;
+//    }
+//
+//    public NewMusicFeatures findById(String id) {
+//        NewMusicFeatures newMusicFeaturesFromBackend = newMusicFeaturesRepository.findById(id)
+//                .map(newmusicfeatures -> new NewMusicFeatures(newmusicfeatures))
+//                .orElse(null);
+//        return newMusicFeaturesFromBackend;
+//    }
+//
+//    /**
+//     * @param songId the songId to use
+//     * @return the song info
+//     */
+//
+//
+//
+//}
