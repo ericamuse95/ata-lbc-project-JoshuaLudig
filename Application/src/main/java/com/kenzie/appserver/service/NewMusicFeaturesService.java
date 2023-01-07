cecc9592a409078d9428a4e93c8ecf91c3414401
JoshuaLudig <joshludig45@yahoo.com>
2023-01-07 02:04:28 -0800

    Made changes to NewMusicFeaturesService class. It is still WIP

--- Application/src/main/java/com/kenzie/appserver/service/NewMusicFeaturesService.java
+++ Application/src/main/java/com/kenzie/appserver/service/NewMusicFeaturesService.java
@@ -1,34 +1,33 @@
-//package com.kenzie.appserver.service;
-//
-//
-//import com.kenzie.appserver.repositories.model.NewMusicFeaturesRepository;
-//import com.kenzie.appserver.service.model.NewMusicFeatures;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class NewMusicFeaturesService {
-//
-//    private NewMusicFeaturesRepository newMusicFeaturesRepository;
-//
-//    /**
-//     * @param newMusicFeaturesRepository the repository to use
-//     */
-//    public NewMusicFeaturesService(NewMusicFeaturesRepository newMusicFeaturesRepository) {
-//        this.newMusicFeaturesRepository = newMusicFeaturesRepository;
-//    }
-//
-//    public NewMusicFeatures findById(String id) {
-//        NewMusicFeatures newMusicFeaturesFromBackend = newMusicFeaturesRepository.findById(id)
-//                .map(newmusicfeatures -> new NewMusicFeatures(newmusicfeatures))
-//                .orElse(null);
-//        return newMusicFeaturesFromBackend;
-//    }
-//
-//    /**
-//     * @param songId the songId to use
-//     * @return the song info
-//     */
-//
-//
-//
-//}
+package com.kenzie.appserver.service;
+
+
+import com.kenzie.appserver.repositories.model.NewMusicFeaturesRepository;
+import com.kenzie.appserver.service.model.NewMusicFeatures;
+import org.springframework.stereotype.Service;
+import com.kenzie.appserver.repositories.model.NewMusicFeaturesRecord;
+
+@Service
+public class NewMusicFeaturesService {
+
+    private NewMusicFeaturesRepository newMusicFeaturesRepository;
+
+    /**
+     * @param newMusicFeaturesRepository the repository to use
+     */
+    public NewMusicFeaturesService(NewMusicFeaturesRepository newMusicFeaturesRepository) {
+        this.newMusicFeaturesRepository = newMusicFeaturesRepository;
+    }
+
+    public NewMusicFeatures addNewFeature(NewMusicFeatures newMusicFeatures) {
+        NewMusicFeaturesRecord newMusicFeaturesRecord = new NewMusicFeaturesRecord();
+        newMusicFeaturesRecord.setFeatureId(newMusicFeatures.getFeatureId());
+        newMusicFeaturesRecord.setFeatureDescription(newMusicFeatures.getFeatureDescription());
+        newMusicFeaturesRecord.setFeatureImage(newMusicFeatures.getFeatureImage());
+        newMusicFeaturesRecord.setFeatureLink(newMusicFeatures.getFeatureLink());
+        newMusicFeaturesRecord.setFeatureName(newMusicFeatures.getFeatureName());
+        newMusicFeaturesRecord.setSongId(newMusicFeatures.getSongId());
+        newMusicFeaturesRepository.save(newMusicFeaturesRecord);
+        return newMusicFeatures;
+    }
+
+}
