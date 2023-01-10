9c44a2e312e2ae11a39b0e8f61f670ddee1ed0c5
Erica Muse <erica.muse@mykenzie.snhu.edu>
2023-01-09 20:03:00 -0800

    Deleted Example files and unused imports.

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/SongDownloadControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/SongDownloadControllerTest.java
@@ -13,9 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.hamcrest.Matchers.hasSize;
-
-import java.util.UUID;
-
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
